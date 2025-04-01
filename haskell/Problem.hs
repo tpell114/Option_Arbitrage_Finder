f9893635c72708d73fd93ddc0b90dba930c625c7
tpell114 <tpell114@mtroyal.ca>
2025-03-31 18:17:51 -0600

    added a few more functions

--- haskell/Problem.hs
+++ haskell/Problem.hs
@@ -36,6 +36,52 @@ removeLeg prob = case combination prob of
     (_:xs) -> prob {combination = xs}
 
 -- Calculate the total cost of the combination
+-- Returns cost (debit/credit)
 getTotalCost :: Problem -> Double
 getTotalCost prob = sum (map getCost (combination prob))
+
+getCriticalPrices :: Problem -> Set.Set Double
+getCriticalPrices prob = Set.fromList ([0.0, 10000.0] ++ [strike (legOption leg) | leg <- combination prob])
+  
+-- Calculate payoff at a specific price
+-- Price -> Problem -> Payoff
+calculatePayoffAt :: Double -> Problem -> Double
+calculatePayoffAt price prob = 
+    let initialPayoff = negate (getTotalCost prob)  -- start with -cost
+    in initialPayoff + sum [legPayoff leg | leg <- combination prob]
+    where
+        legPayoff leg =
+            let opt = legOption leg
+                intrinsicValue = case optionType opt of
+                    Call -> max 0.0 (price - strike opt)
+                    Put -> max 0.0 (strike opt - price)
+            in case position leg of
+                Long -> intrinsicValue
+                Short -> negate intrinsicValue
+
+-- Get all possible legs that can be added to the combination
+-- Problem -> Posible moves
+getPossibleMoves :: Problem -> [OptionLeg]
+getPossibleMoves prob = 
+    if length (combination prob) >= 4
+    then []  -- Maximum 4 legs
+    else concat [getLegsForStrike strikePrice | strikePrice <- getAllStrikes (chainData prob)]
+    where
+        getLegsForStrike strikePrice = 
+            let (maybeCall, maybePut) = getOptionsAtStrike strikePrice (chainData prob)
+                usedOptions = map legOption (combination prob)
+                
+                callLegs = case maybeCall of
+                    Nothing -> []
+                    Just opt -> if opt `elem` usedOptions
+                               then []
+                               else [newLeg opt Long, newLeg opt Short]
+                               
+                putLegs = case maybePut of
+                    Nothing -> []
+                    Just opt -> if opt `elem` usedOptions
+                              then []
+                              else [newLeg opt Long, newLeg opt Short]
+            in callLegs ++ putLegs
+
 
